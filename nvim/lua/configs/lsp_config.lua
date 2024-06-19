707d13a9561934704ecbe594014064d208da632f
jbeni867 <jordybeni867@gmail.com>
2024-06-19 15:04:41 -0500

    Bug fix where keymaps weren't being attatched to lsp's

--- nvim/lua/configs/lsp_config.lua
+++ nvim/lua/configs/lsp_config.lua
@@ -16,6 +16,6 @@ end
 
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-require("lspconfig").lua_ls.setup({{ on_attach = on_attach, capabilities = capabilities }})
-require("lspconfig").csharp_ls.setup({{ on_attach = on_attach, capabilities = capabilities }})
-require("lspconfig").basedpyright.setup({{ on_attach = on_attach, capabilities = capabilities }})
+require("lspconfig").lua_ls.setup({ on_attach = on_attach, capabilities = capabilities })
+require("lspconfig").csharp_ls.setup({ on_attach = on_attach, capabilities = capabilities })
+require("lspconfig").basedpyright.setup({ on_attach = on_attach, capabilities = capabilities })
