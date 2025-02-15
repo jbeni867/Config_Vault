34f880eb76f7c496a9a8e1d192cde97417c7ad5a
Jordy Benitez <jbenitez@gmail.com>
2025-02-14 22:35:06 -0600

    Added code in config.el for emacsclient
    Added package to install script

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -75,3 +75,8 @@
 ;;
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
+
+(after! persp-mode
+  (setq persp-emacsclient-init-frame-behaviour-override
+        `(+workspace-current-name))
+  )
--- Setup/linux_setup.sh
+++ Setup/linux_setup.sh
@@ -2,7 +2,7 @@
 
 # Update and install essential packages
 sudo apt-get update && sudo apt-get upgrade -y
-sudo apt-get install git dotnet-sdk-8.0 stow emacs ripgrep fd-find make cmake fzf lf nextcloud-desktop kitty build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev curl libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev texlive dvipng texlive-latex-extra libtool libvterm-dev software-properties-common tmux -y
+sudo apt-get install git dotnet-sdk-8.0 stow emacs ripgrep fd-find make cmake fzf lf nextcloud-desktop kitty build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev curl libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev texlive dvipng texlive-latex-extra libtool libvterm-dev software-properties-common tmux ttf-mscorefonts-installer -y
 
 # Install Tailscale
 curl -fsSL https://tailscale.com/install.sh | sh
