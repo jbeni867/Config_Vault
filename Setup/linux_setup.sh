2e0c6fa925a67b2fccd0196d176ba002bc7a9b19
jbeni867 <jordybeni867@gmail.com>
2025-05-09 02:02:02 -0500

    Update linux_setup.sh

--- Setup/linux_setup.sh
+++ Setup/linux_setup.sh
@@ -4,13 +4,11 @@
 sudo apt-get update && sudo apt-get upgrade -y
 sudo apt-get install git dotnet-sdk-8.0 stow emacs ripgrep fd-find make cmake fzf lf nextcloud-desktop kitty build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev curl libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev texlive dvipng texlive-latex-extra libtool libvterm-dev software-properties-common tmux ttf-mscorefonts-installer -y
 
-# Install Tailscale
-curl -fsSL https://tailscale.com/install.sh | sh
-
-# Install Neovim
-sudo add-apt-repository ppa:neovim-ppa/unstable -y
-sudo apt-get update
-sudo apt-get install neovim -y
+# Clone and apply dotfiles from Config_Vault
+git clone https://github.com/jbeni867/Config_Vault ~/Config_Vault
+cd ~/Config_Vault
+stow --adopt .
+git restore .
 
 # Install pyenv and configure Python
 curl -fsSL https://pyenv.run | bash
@@ -20,11 +18,13 @@ bash --login -i -c 'pyenv install 3.12.0 && pyenv global 3.12.0'
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
 bash --login -i -c 'nvm install node && nvm use node'
 
-# Clone and apply dotfiles from Config_Vault
-git clone https://github.com/jbeni867/Config_Vault ~/Config_Vault
-cd ~/Config_Vault
-stow --adopt .
-git restore .
+# Install Neovim
+sudo add-apt-repository ppa:neovim-ppa/unstable -y
+sudo apt-get update
+sudo apt-get install neovim -y
+
+# Install Tailscale
+curl -fsSL https://tailscale.com/install.sh | sh
 
 # Install Doom Emacs
 git clone https://github.com/hlissner/doom-emacs ~/.emacs.d
@@ -34,7 +34,8 @@ git clone https://github.com/hlissner/doom-emacs ~/.emacs.d
 # Install Tmux Plugin Manager
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
-mkdir ~/Development
-
 # Install NordVPN
 sh <(curl -sSf https://downloads.nordcdn.com/apps/linux/install.sh)
+
+# Create Dev Folder
+mkdir ~/Development
