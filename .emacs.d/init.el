2e4e8b9745b33ea1bed8624913df04747e750d2a
jbenitez <jordybeni867@gmail.com>
2026-02-22 21:54:31 -0600

    Added LSP to the config; Too much to recap

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -10,6 +10,11 @@
 (setq display-line-numbers-type 'relative)
 (column-number-mode)
 
+(set-default-coding-systems 'utf-8)
+(prefer-coding-system 'utf-8)
+(set-terminal-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+
 (defvar void/default-font-size 115)
 (defvar void/my-ui-font (if (eq system-type 'windows-nt) "Segoe UI" "Cantarell"))
 
@@ -26,9 +31,9 @@
 		    :weight 'regular)
 
 (org-babel-do-load-languages
-  'org-babel-load-languages
-  '((emacs-lisp . t)
-    (python . t)))
+ 'org-babel-load-languages
+ '((emacs-lisp . t)
+   (python . t)))
 
 (setq org-confirm-babel-evaluate nil)
 
@@ -64,19 +69,19 @@
 
 ;; Set faces for heading levels
 (with-eval-after-load 'org-faces
-    (dolist (face '((org-level-1 . 1.2)
-		    (org-level-2 . 1.1)
-		    (org-level-3 . 1.05)
-		    (org-level-4 . 1.0)
-		    (org-level-5 . 1.1)
-		    (org-level-6 . 1.1)
-		    (org-level-7 . 1.1)
-		    (org-level-8 . 1.1)))
+  (dolist (face '((org-level-1 . 1.2)
+		  (org-level-2 . 1.1)
+		  (org-level-3 . 1.05)
+		  (org-level-4 . 1.0)
+		  (org-level-5 . 1.1)
+		  (org-level-6 . 1.1)
+		  (org-level-7 . 1.1)
+		  (org-level-8 . 1.1)))
     (set-face-attribute (car face) nil :font void/my-ui-font :weight 'regular :height (cdr face))))
 
 ;; Ensure that anything that should be fixed-pitch in Org files appears that way
 (with-eval-after-load 'org
-    (set-face-attribute 'org-block nil :foreground nil :inherit 'fixed-pitch)
+  (set-face-attribute 'org-block nil :foreground nil :inherit 'fixed-pitch)
     (set-face-attribute 'org-code nil   :inherit '(shadow fixed-pitch))
     (set-face-attribute 'org-table nil   :inherit '(shadow fixed-pitch))
     (set-face-attribute 'org-verbatim nil :inherit '(shadow fixed-pitch))
@@ -158,15 +163,19 @@
 
 (use-package general
   :ensure t
-  :init
+  :after (evil which-key)
+  :config
   (general-create-definer void/leader-keys
     :keymaps '(normal insert visual emacs)
     :prefix "SPC"
     :global-prefix "C-SPC")
-  :config
+  
   (void/leader-keys
     "t"  '(:ignore t :which-key "toggles")
-    "tt" '(counsel-load-theme :which-key "choose theme")))
+    "tt" '(counsel-load-theme :which-key "choose theme")
+    ;; New bindings for docs
+    "k"  '(:ignore t :which-key "docs")
+    "ki" '(lsp-ui-doc-show :which-key "show hover doc")))
 
 (use-package evil
   :ensure t
@@ -176,7 +185,6 @@
   (setq evil-want-keybinding nil)
   (setq evil-want-C-u-scroll t)
   (setq evil-want-C-i-jump nil)
-  :hook (evil-mode . void/evil-hook)
   :config
   (evil-mode 1)
   (define-key evil-insert-state-map (kbd "C-g") 'evil-normal-state)
@@ -195,10 +203,10 @@
 
 (use-package hydra)
 (defhydra hydra-text-scale (:timeout 6)
-	   "scale text"
-	   ("j" text-scale-increase "in")
-	   ("k" text-scale-decrease "out")
-	   ("f" nil "finished" :exit t))
+  "scale text"
+  ("j" text-scale-increase "in")
+  ("k" text-scale-decrease "out")
+  ("f" nil "finished" :exit t))
 
 (void/leader-keys
   "ts" '(hydra-text-scale/body :which-key "scale text"))
@@ -214,7 +222,7 @@
   (when (file-directory-p "~/Development")
     (setq projectile-project-search-path '("~/Development")))
   (setq projectile-switch-project-action #'projectile-dired))
-; TODO: Add info about <M-o> giving more info on additional operations
+  ; TODO: Add info about <M-o> giving more info on additional operations
 (use-package counsel-projectile
   :config (counsel-projectile-mode))
 
@@ -232,7 +240,7 @@
   (setq org-agenda-start-with-log-mode t)
   (setq org-log-done 'time)
   (setq org-log-into-drawer t)
-
+  
   (require 'org-tempo)
   (add-to-list 'org-structure-template-alist '("sh" . "src shell"))
   (add-to-list 'org-structure-template-alist '("el" . "src emacs-lisp"))
@@ -240,12 +248,76 @@
   (add-to-list 'org-structure-template-alist '("cs" . "src csharp"))
   (setq org-src-fontify-natively t)
   (setq org-src-tab-acts-natively t)
-
+  
   (use-package org-bullets
     :after org
     :hook (org-mode . org-bullets-mode)
     :custom
     (org-bullets-bullet-list '("◉" "○" "●" "○" "●" "○" "●")))
-
+  
   (use-package visual-fill-column
     :hook (org-mode . void/org-mode-visual-fill)))
+
+(use-package lsp-mode
+  :commands (lsp lsp-deferred)
+  :init (setq lsp-keymap-prefix "SPC l")
+  :hook (
+	 (csharp-mode . lsp-deferred)
+	 (web-mode . lsp-deferred)
+	 (python-mode . lsp-deferred)
+	 (js-mode . lsp-deferred))
+  :config
+  (lsp-enable-which-key-integration t)
+  (void/leader-keys
+    "l" '(:keymap lsp-command-map :which-key "lsp")))
+
+(defun void/lsp-mode-setup ()
+  (setq lsp-header-breadcrumb-segments '(path-up-to-project file symbols))
+  (lsp-headerline-breadcrumb-mode))
+
+(add-hook 'lsp-mode-hook #'void/lsp-mode-setup)
+
+(use-package company
+  :after lsp-mode
+  :hook (lsp-mode . company-mode)
+  :bind (:map company-active-map
+              ("<tab>" . company-complete-selection)
+              ("TAB"   . company-complete-selection)
+              :map lsp-mode-map
+              ("<tab>" . company-indent-or-complete-common)
+              ("TAB"   . company-indent-or-complete-common))
+  :custom
+  (company-minimum-prefix-length 1)
+  (company-idle-delay 0.1))
+
+(use-package company-box
+  :hook (company-mode . company-box-mode))
+
+(use-package lsp-ui
+  :hook (lsp-mode . lsp-ui-mode)
+  :custom
+  ;; Sideline configuration
+  (lsp-ui-sideline-enable t)
+  (lsp-ui-sideline-show-diagnostics t)
+  (lsp-ui-sideline-show-hover nil)
+  
+  ;; Documentation popup configuration (the "hover" effect)
+  (lsp-ui-doc-enable t)
+  (lsp-ui-doc-show-with-cursor nil)
+  (lsp-ui-doc-show-with-mouse nil)
+  (lsp-ui-doc-position 'at-point))
+
+(use-package flycheck
+  :ensure t
+  :init (global-flycheck-mode))
+
+(use-package lsp-treemacs
+  :after lsp)
+
+(use-package lsp-ivy)
+
+(use-package evil-nerd-commenter
+  :bind ("M-/" . evilnc-comment-or-uncomment-lines)
+  :config
+  (void/leader-keys
+    "kc" '(evilnc-comment-or-uncomment-lines :which-key "comment lines")))
